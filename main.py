65c8e38b4e0f15b801d997b9d370817d65dd044b
YAr <yaroslav.mindra@gmail.com>
2023-06-14 17:51:54 +0300

    Клас player було додано

--- main.py
+++ main.py
@@ -27,8 +27,21 @@ class Game_sprite(pygame.sprite.Sprite):
     def show(self):
         window.blit(self.image, (self.rect.x, self.rect.y ))
 
+class Player(Game_sprite):
+    def __init__(self, x, y, width, height, image):
+        super().__init__(x, y, width, height, image)
+        self.speedx = 0
+        self.speedy = 0
+
+    def update(self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
         
-player = Game_sprite(4, 4, 50, 20, r"images\back.jpg")
+
+
+player = Player(4, 4, 70, 40, r"images/souris_02-300x258.png")
+enemy = Game_sprite(50, 60, 100, 100, r"images/Eurasian_Lynx.png")
+finish = Game_sprite(100, 200, 70, 50, r"images/cheese-1.png")
 
 
 game = True
@@ -37,10 +50,32 @@ while game == True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
+        if level == 1:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_d:
+                    player.speedx = 5
+                if event.key == pygame.K_a:
+                    player.speedx = -5
+                if event.key == pygame.K_w:
+                    player.speedy = -5
+                if event.key == pygame.K_s:
+                    player.speedy = 5
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_d:
+                    player.speedx = 0
+                if event.key == pygame.K_a:
+                    player.speedx = 0
+                if event.key == pygame.K_w:
+                    player.speedy = 0
+                if event.key == pygame.K_s:
+                    player.speedy = 0
 
     if level == 1:
         window.blit(back, (0, 0))
         player.show()
+        player.update()
+        enemy.show()
+        finish.show()
 
     clock.tick(FPS)
     pygame.display.update()
